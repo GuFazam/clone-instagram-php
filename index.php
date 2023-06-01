1e551d9386fad310124b03e7e4296b5c5229322f
Gustavo H. Fazam <112525498+GuFazam@users.noreply.github.com>
2023-06-01 19:36:59 -0300

    .

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     <title>Insta Clone</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-
 </head>
 <body>
     <div class="container-fluid g-0">
@@ -35,12 +34,19 @@
                     </div>
                 </div>
                 <img src="img/<?php echo $fotos[$i]; ?>" class="img-fluid">
-                <p><?php echo $legendas[$i]; ?></p>
+                <div class="row">
+                    <div class="col-12">
+                    <i class="bi bi-heart"></i>
+                    <i class="bi bi-chat"></i>
+                    </div>
+                    <div class="col-12">
+                        <p><?php echo $legendas[$i]; ?></p>
+                    </div>
+                </div>
             </div>
         <?php
         endfor; //significa o fechamento do for
         ?>
     </div>
 </body>
-
 </html>
